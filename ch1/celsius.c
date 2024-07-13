7666d9da586dddbbb84ddea30d4741df966e8e14
Kate Crawford <93723053+codewithkate@users.noreply.github.com>
2024-07-13 18:27:05 -0400

    Add programs for 2024-06-31

---
+++ ch1/celsius.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+print Fahrenheit-Celsius table
+for for fahr = 0, 25, ..., 400
+*/
+
+int main (void) {
+	float fahr, celsius;
+	float lower, upper, step;
+
+	lower = 0;			/* lower limit of temperature scale */
+	upper = 400;		/* upper limit */
+	step = 25;			/* step size */
+	
+	printf("Fahrenheit-Celsius Table\n");
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%5.0f %10.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
---
+++ test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+	int result;
+
+	result = 5.0/9.0;
+
+	printf("%d\n", result);
+}
